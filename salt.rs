74c6e49a35f50c1b3f6fda01c95ae1f917f9fa7f
John Paton <JWJPaton@users.noreply.github.com>
2023-02-03 12:51:34 +0000

    Create salt.rs

---
+++ salt.rs
@@ -0,0 +1,19 @@
+/*******************************************************************************
+* Create simple tuple utterance of (handle: string, audio file)                *
+* Data is an array[26] of vec's of utterance tuples. <-(does this make sense?) *
+*                                                                              *
+* request queue is a vec of strings                                            *                                                            
+* Audio file should be a stripped wav (to save space if poss)                  *
+*                                                                              *                                                             
+*                                                                              *
+*******************************************************************************/
+                                                             
+fn pub request_queue() -> /*vec of strings or tuple?*/                                                             
+                                                             
+fn pub read_text(handle: string)->(/* audio file*/)
+{
+\\ if utterance exists return audio file
+  
+\\ if utterance does not exist add letters to audio queue and add request to request queue 
+
+}
