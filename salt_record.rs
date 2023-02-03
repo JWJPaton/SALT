d711ad1132d4ed34efa32f90cae0a70e1681da7d
John Paton <JWJPaton@users.noreply.github.com>
2023-02-03 12:55:19 +0000

    Create salt_record.rs

---
+++ salt_record.rs
@@ -0,0 +1,3 @@
+// enables users to record their own utterances
+// device spells out the required utterance, records the reply then may trim quiet from start and end
+// it then places it in the main lookup table
